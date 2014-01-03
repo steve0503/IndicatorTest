7de491e916d3175d43a118731f3c5bcb4978d8e7
steve0503 <leehr76@gmail.com>
2014-01-03 15:53:04 +0900

    Indicator Test
    Done

--- IndicatorTest/ViewController.m
+++ IndicatorTest/ViewController.m
@@ -10,14 +10,47 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *indicator1;
+
+@property (strong, nonatomic) UIActivityIndicatorView *indicator2;
+
 @end
 
 @implementation ViewController
+
+@synthesize indicator1;
+
+@synthesize indicator2;
+
+- (IBAction)startIndicator:(id)sender {
+    
+    [indicator1 startAnimating];
+    
+    [indicator2 performSelector:@selector(startAnimating) withObject:nil afterDelay:0.3];
+    
+}
+
+
+- (IBAction)stopIndicator:(id)sender {
+    
+    [indicator1 stopAnimating];
+    
+    [indicator2 performSelector:@selector(stopAnimating) withObject:nil afterDelay:0.7];
+}
+
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    indicator2 = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    indicator2.hidesWhenStopped = YES;
+    indicator2.center = CGPointMake(160,200);
+    
+    [self.view addSubview:indicator2];
+    
 }
 
 - (void)didReceiveMemoryWarning
